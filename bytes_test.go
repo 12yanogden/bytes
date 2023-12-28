a95deb22278e17e67308957b9fa6564ee0649c7f
Ryan Ogden <12yanogden@gmail.com>
2023-12-28 16:40:06 -0500

    add helper functions and tests

---
+++ bytes_test.go
@@ -0,0 +1,66 @@
+package bytes
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Explode a byte slice, where the delimiter is at the beginning, into byte slices
+func TestExplodeWithDelimeterBeginning(t *testing.T) {
+	byteSlice := []byte{'a', 'b', 'c'}
+	delimiter := []byte{'a', 'b'}
+	expected := [][]byte{{'c'}}
+	actual := Explode(byteSlice, delimiter)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("\nExpected: " + SlicesToPrintString(expected) + "\nActual: " + SlicesToPrintString(actual))
+	}
+}
+
+// Explode a byte slice, where the delimiter is at the end, into byte slices
+func TestExplodeWithDelimeterEnding(t *testing.T) {
+	byteSlice := []byte{'a', 'b', 'c'}
+	delimiter := []byte{'b', 'c'}
+	expected := [][]byte{{'a'}}
+	actual := Explode(byteSlice, delimiter)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("\nExpected: " + SlicesToPrintString(expected) + "\nActual: " + SlicesToPrintString(actual))
+	}
+}
+
+// Explode a byte slice, which is equal to the delimiter, into byte slices
+func TestExplodeWithDelimeterOnly(t *testing.T) {
+	byteSlice := []byte{'a', 'b'}
+	delimiter := []byte{'a', 'b'}
+	expected := [][]byte{}
+	actual := Explode(byteSlice, delimiter)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("\nExpected: " + SlicesToPrintString(expected) + "\nActual: " + SlicesToPrintString(actual))
+	}
+}
+
+// Explode a byte slice into byte slices with a single byte delimiter
+func TestExplodeWithSingleByteDelimiter(t *testing.T) {
+	byteSlice := []byte{'a', 'b', ',', ' ', 'c'}
+	delimiter := []byte{','}
+	expected := [][]byte{{'a', 'b'}, {' ', 'c'}}
+	actual := Explode(byteSlice, delimiter)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("\nExpected: " + SlicesToPrintString(expected) + "\nActual: " + SlicesToPrintString(actual))
+	}
+}
+
+// Explode a byte slice into byte slices with a multi-byte delimiter
+func TestExplodeWithMultipleByteDelimiter(t *testing.T) {
+	byteSlice := []byte{'a', 'b', ',', ' ', 'c'}
+	delimiter := []byte{',', ' '}
+	expected := [][]byte{{'a', 'b'}, {'c'}}
+	actual := Explode(byteSlice, delimiter)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("\nExpected: " + SlicesToPrintString(expected) + "\nActual: " + SlicesToPrintString(actual))
+	}
+}
